810daf25b3b1a08b990cf5292d43cbad34b3f92a
Abstrack <Armon_ah05@outlook.com>
2022-05-20 18:33:42 -0400

    pushing

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const { animals } = require("./data/animals");
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require("fs");
-const path = require("path");
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -11,24 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
